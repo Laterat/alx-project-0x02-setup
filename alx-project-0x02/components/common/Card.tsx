45e0646081d76919020c8e46490978aa71bd2eaf
Latera Mekonnen <mekonnenlatera@gmail.com>
2025-06-26 22:04:59 +0300

    card compoent and interface

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,15 @@
+
+
+import { CardProps } from "@/interfaces";
+import React from "react";
+
+const Card: React.FC<CardProps>=({title,content})=>{
+    return(
+         <div>
+            <h2>{title}</h2>
+            <p>{content}</p>
+         </div>
+    );
+}
+
+export default Card;
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/layout/Header";
-
+import Card from "@/components/common/Card";
 
 
 
@@ -7,6 +7,7 @@ const Home: React.FC=()=>{
     return(
         <>  <Header/>
             <h1>Home</h1>
+            <Card title="This is home" content=" the content"/>
         </>
         
     );
