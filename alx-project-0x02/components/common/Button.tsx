7fd8cb914f7267cad0596a6a6e73b6b0a61fc217
Latera Mekonnen <mekonnenlatera@gmail.com>
2025-06-29 11:36:41 +0300

    button

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -2,9 +2,12 @@
 
 import { ButtonProps } from "@/interfaces";
 
-const Button:React.FC<ButtonProps>=()=>{
+const Button:React.FC<ButtonProps>=({size,shape})=>{
     return(
-        <></>
+        <>
+           <button className={`text-${size} ${shape} text-white bg-gray-500 w-2xl hover:bg-gray-600  `}>{size} {shape}</button>
+           
+        </>
     );
 }
 
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/common/Button";
 import Header from "@/components/layout/Header";
 
 
@@ -7,7 +8,16 @@ const About: React.FC=()=>{
     return(
         <>
            <Header/>
-           <h1>About</h1>
+           <div className="p-2">
+              <h1 className="text-2xl font-semibold">About Page</h1>
+              <h2 className="text-xl font-semibold">Small</h2>
+                 <div className="flex space-x-3">
+                    <Button size="sm" shape="rounded-sm"/>
+                    <Button size="base" shape="rounded-md"/>
+                    <Button size="lg" shape="rounded-full"/>
+                    
+                 </div>
+            </div>
         </>
     );
 }
