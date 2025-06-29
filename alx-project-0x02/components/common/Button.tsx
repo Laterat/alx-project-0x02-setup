0f73b5c3ab65d3f3419cd0356fde489e32476005
Latera Mekonnen <mekonnenlatera@gmail.com>
2025-06-29 11:44:29 +0300

    button

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -3,9 +3,16 @@
 import { ButtonProps } from "@/interfaces";
 
 const Button:React.FC<ButtonProps>=({size,shape})=>{
+
+     const sizeMap: Record<ButtonProps['size'], string>={
+           small: "text-sm text-blue-500 ",
+           medium: "text-base",
+           large: "text-lg",
+    }
+
     return(
         <>
-           <button className={`text-${size} ${shape} text-white bg-gray-500 w-2xl hover:bg-gray-600  `}>{size} {shape}</button>
+           <button className={`${sizeMap[size]} ${shape} text-white bg-gray-500 w-2xl hover:bg-gray-600  `}>{size} {shape}</button>
            
         </>
     );
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -2,9 +2,9 @@ import Button from "@/components/common/Button";
 import Header from "@/components/layout/Header";
 
 
-
-
 const About: React.FC=()=>{
+
+   
     return(
         <>
            <Header/>
@@ -12,9 +12,9 @@ const About: React.FC=()=>{
               <h1 className="text-2xl font-semibold">About Page</h1>
               <h2 className="text-xl font-semibold">Small</h2>
                  <div className="flex space-x-3">
-                    <Button size="sm" shape="rounded-sm"/>
-                    <Button size="base" shape="rounded-md"/>
-                    <Button size="lg" shape="rounded-full"/>
+                    <Button size="small" shape="rounded-sm"/>
+                    <Button size="medium" shape="rounded-md"/>
+                    <Button size="large" shape="rounded-full"/>
                     
                  </div>
             </div>
